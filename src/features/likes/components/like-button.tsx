af4be3e3abe800329b25b18d41820418a4699bc6
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-12-12 12:45:34 +0900

    ✨ いいね楽観的UI更新機能実装

--- src/features/likes/components/like-button.tsx
+++ src/features/likes/components/like-button.tsx
@@ -3,10 +3,11 @@
 import { Form, TextField } from '@/components/ui'
 import { likeAction } from '@/features/likes/action/like-action'
 import { likeFormSchema } from '@/features/likes/types/schema/like-form-schema'
+import { useUser } from '@clerk/nextjs'
 import { getFormProps, getInputProps, useForm } from '@conform-to/react'
 import { getZodConstraint, parseWithZod } from '@conform-to/zod'
-import { IconHeart } from 'justd-icons'
-import { useActionState } from 'react'
+import { IconHeart, IconHeartFill } from 'justd-icons'
+import { useActionState, useOptimistic } from 'react'
 
 type LikeButtonProps = {
   postId: string
@@ -14,13 +15,34 @@ type LikeButtonProps = {
 }
 
 export const LikeButton = ({ postId, initialLikes }: LikeButtonProps) => {
-  const [lastResult, action, isPending] = useActionState(likeAction, null)
+  const { user } = useUser()
+
+  const [optimisticLike, setOptimisticLike] = useOptimistic<
+    { likeCount: number; isLiked: boolean },
+    void
+  >(
+    {
+      likeCount: initialLikes.length,
+      isLiked: user?.id ? initialLikes.includes(user.id) : false,
+    },
+    (currentState) => ({
+      likeCount: currentState.isLiked
+        ? currentState.likeCount - 1
+        : currentState.likeCount + 1,
+      isLiked: !currentState.isLiked,
+    }),
+  )
+
+  const [lastResult, action] = useActionState(likeAction, null)
 
   const [form, fields] = useForm({
     constraint: getZodConstraint(likeFormSchema),
     lastResult,
     onValidate({ formData }) {
       return parseWithZod(formData, { schema: likeFormSchema })
+    },
+    onSubmit() {
+      setOptimisticLike()
     },
     defaultValue: {
       postId,
@@ -30,13 +52,15 @@ export const LikeButton = ({ postId, initialLikes }: LikeButtonProps) => {
   return (
     <Form {...getFormProps(form)} action={action}>
       <TextField {...getInputProps(fields.postId, { type: 'hidden' })} />
-      <button
-        type="submit"
-        className="flex items-center gap-2"
-        disabled={isPending}
-      >
-        <IconHeart className="size-5" />
-        <span>{initialLikes.length}</span>
+      <button type="submit" className="flex items-center gap-2">
+        {optimisticLike.isLiked ? (
+          <IconHeartFill className="size-5 text-red-500" />
+        ) : (
+          <IconHeart className="size-5" />
+        )}
+        <span className={optimisticLike.isLiked ? 'text-red-500' : ''}>
+          {optimisticLike.likeCount}
+        </span>
       </button>
     </Form>
   )
